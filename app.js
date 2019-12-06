734b6585768559d76b68e05c11afddb280a25d9e
Laridff <lariffernandes@hotmail.com>
2019-12-06 16:11:11 +0000

    changes 3

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ router.post('/post/json', function(req, res) {
     xmlFileToJs('LikeanAngel.xml', function(err, result) {
       if (err) throw (err);
       //This is where you pass on information from the form inside index.html in a form of JSON and navigate through our JSON (XML) file to create a new entree object
-      result.cafemenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price}); //If your XML elements are differet, this is where you have to change to your own element names
+      result.bookservice.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price}); //If your XML elements are differet, this is where you have to change to your own element names
       //Converting back to our original XML file from JSON
       jsToXmlFile('LikeanAngel.xml', result, function(err) {
         if (err) console.log(err);
