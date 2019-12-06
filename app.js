9665564c2863f90fa109872744c68ff30c44b249
Laridff <lariffernandes@hotmail.com>
2019-12-06 15:39:42 +0000

    delete changes

--- app.js
+++ app.js
@@ -57,17 +57,17 @@ router.post('/post/json', function(req, res) {
 
   // Function to read in a JSON file, add to it & convert to XML
   function appendJSON(obj) {
-    console.log(obj);
     // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
     xmlFileToJs('LikeanAngel.xml', function(err, result) {
       if (err) throw (err);
-      result.cafemenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price});
-      console.log(result);
+      //This is where you pass on information from the form inside index.html in a form of JSON and navigate through our JSON (XML) file to create a new entree object
+      result.cafemenu.section[obj.sec_n].entree.push({'item': obj.item, 'price': obj.price}); //If your XML elements are differet, this is where you have to change to your own element names
+      //Converting back to our original XML file from JSON
       jsToXmlFile('LikeanAngel.xml', result, function(err) {
         if (err) console.log(err);
       })
     })
-  }
+  };
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
@@ -77,6 +77,29 @@ router.post('/post/json', function(req, res) {
 
 });
 
+// POST request to add to JSON & XML files
+router.post('/post/delete', function(req, res) {
+
+  // Function to read in a JSON file, add to it & convert to XML
+  function deleteJSON(obj) {
+    // Function to read in XML file, convert it to JSON, delete the required object and write back to XML file
+    xmlFileToJs('LikeanAngels.xml', function(err, result) {
+      if (err) throw (err);
+      //This is where we delete the object based on the position of the section and position of the entree, as being passed on from index.html
+      delete result.cafemenu.section[obj.section].entree[obj.entree];
+      //This is where we convert from JSON and write back our XML file
+      jsToXmlFile('LikeanAngel.xml', result, function(err) {
+        if (err) console.log(err);
+      })
+    })
+  }
+
+  // Call appendJSON function and pass in body of the current POST request
+  deleteJSON(req.body);
+
+});
+
+//This is where we as the server to be listening to user with a specified IP and Port
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
