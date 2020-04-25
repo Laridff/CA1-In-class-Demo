36e9199fc5bd9d07d08d1a7b01be53c9739f54dc
laridff <lariffernandes@hotmail.com>
2020-04-26 00:12:02 +0530

    add db connection details

--- connect.js
+++ connect.js
@@ -8,7 +8,7 @@ require("custom-env").env();
 module.exports.connect = () => {
   const { DB_CONN_STRING } = process.env;
   return mongoose.connect(
-    "mongodb://host:59105/dbname",
+    "mongodb://user:user@123@ds259105.mlab.com:59105/test",
     {
       useNewUrlParser: true,
       serverSelectionTimeoutMS: 5000,
